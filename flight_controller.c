d09d5e7cb2f1ab9607e82fe3aafadc64aae43689
zRhYaN <brygamerbr@gmail.com>
2026-01-27 09:01:23 -0300

    Início do loop principal da máquina de estados

--- flight_controller.c
+++ flight_controller.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 // ==========================================
 //           DEFINIÇÃO DOS ESTADOS
@@ -19,13 +20,33 @@ typedef struct {
     float accel_x, accel_y, accel_z; // Acelerômetro
     float pressure;                  // Barômetro
     float altitude;                  // Calculada a partir da pressão
-    float temperature;               // Opcional
+    float temperature;               // Opcional, pensarei depois se será útil
 } SensorData;
+
+// Definir funções aqui
+// init_sensors();
+// read_sensors();
+// start_engine();
+// start_recovery();
 
 // Variáveis Globais de Controle
 FlightState current_state = STATE_STANDBY;
 
 void main(void){
     printf("Iniciando aviônica...");
-    init_sensors();
+    init_sensors(); // Essa função tealvez também poderia retornar o status de todos os sensores
+                    // E sinalizar um erro caso houvesse um
+    
+    while(1) { // Loop principal do controlador
+        SensorData data = read_sensors(); // Struct recebe as leituras
+
+        switch (current_state){
+        case STATE_STANDBY:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    }
 }
