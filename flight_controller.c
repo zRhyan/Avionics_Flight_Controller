42a202d9784ffa0e3dba0355faa3e7f2b9581204
zRhYaN <brygamerbr@gmail.com>
2026-01-30 09:37:19 -0300

    v0.4 - Início da implementação dos algoritmos mais robustos no COASTING e APOGEE

--- flight_controller.c
+++ flight_controller.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 // ==========================================
 //           DEFINIÇÃO DOS ESTADOS
@@ -41,14 +42,17 @@ void main(void){
     // Preciso de um modo para armazenar as medidas anteriores para montar as condições corretas de mudança de estado.
     // Por enquanto vou utilizar simples structs.
     float memory_accel_z[3] = {0}, memory_bar[6] = {0}, average_pressure[2] = {0};
-    int cycles_1 = 0, cycles_2 = 0;
+    int cycles_1 = 0, cycles_2 = -1;
     while(1) { // Loop principal do controlador
         SensorData data = read_sensors(); // Struct recebe as leituras
-                                          // Estou consierando que essa leitura ocorre a cada 0,1s
+                                          // Estou considerando que essa leitura ocorre a cada 0,1s
         memory_accel_z[cycles_1%3] = data.accel_z;
-        
-        // Quando estiver no COASTING, apenas preenche [3], [4] e [5]
-        if(cycles_2 == 0) memory_bar[cycles_1%3] = data.pressure; // Apenas [0], [1] e [2]
+        memory_bar[1] = data.pressure;
+
+
+        /*
+        // Enquanto não estiver no COASTING (cycles_2 == -1), apenas preenche [0], [1] e [2]
+        if(cycles_2 == -1) memory_bar[cycles_1%3] = data.pressure; // Apenas [0], [1] e [2]
         else memory_bar[cycles_2%3 + 3] = data.pressure;         // Apenas [3], [4] e [5]
 
         if(current_state == STATE_COASTING){
@@ -59,6 +63,9 @@ void main(void){
             }
             else average_pressure[1] = (memory_bar[3] + memory_bar[4] + memory_bar[5])/3;
         }
+        */
+        
+
         switch (current_state){
         
             case STATE_STANDBY:
@@ -79,23 +86,19 @@ void main(void){
                     else flag_coasting = 0;
                 }
                 if(flag_coasting == 3) current_state = STATE_COASTING;
+                cycles_2 = 0; //"Inicia" o ciclo 2
                 break;
 
             case STATE_COASTING:
-                cycles_2++;
-                // Precisa de no mínimo três novas medidas ([3], [4] e [5]) para comparar com a última média
-                if(cycles_2 >= 3){
-                    // O sinal para passar para o próximo estado (apogeu), é accel_z = 0 e a menor pressão.
-                    // O problema é que essas duas medidas são sensíveis e, portanto, são facilmente interferidas por ruídos.
-                    int flag_null_accel = 0;
-                    if(data.accel_z > -0.1 && data.accel_z < 0.1) flag_null_accel = 1;
-                    if(average_pressure[0] < average_pressure[1] && flag_null_accel == 1) {
-                        current_state = STATE_APOGEE;
-                        break; // Já sai do case e armazena as 6 últimas medidas de pressão para estimar a altura máxima
-                    }
-                    // Passar os memory_bar[3], [4] e [5], respectivamente para [0], [1] e [2], para manter average_pressure[0] como a mais antiga
-                    for(int j = 0; j < 3; j++) {memory_bar[j] = memory_bar[j+3];}
+                // 1. Registro das alturas (pressões) com filtro
+                // Adicionar lógica para obter o primeiro valor válido, memory_bar[0];
+                int delta_H_limit;
+                if(abs(memory_bar[0] - memory_bar[1]) <= delta_H_limit) {
+                    // memory_bar[0] - valor validado
+                    // memory_bar[1] - valor em análise
+                    memory_bar[0] = memory_bar[1];
                 }
+                else {memory_bar[0] += delta_H_limit;}
                 
                 break;
             
