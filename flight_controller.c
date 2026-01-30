671adc5072d383352943657092422bea686419a7
zRhYaN <brygamerbr@gmail.com>
2026-01-30 16:09:50 -0300

    v0.4.1 - Implementação da primeira lógica de atualização da variação da altura durante o POWERED_FLIGHT.

--- flight_controller.c
+++ flight_controller.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <math.h>
 
 // ==========================================
 //           DEFINIÇÃO DOS ESTADOS
@@ -41,29 +42,14 @@ void main(void){
     
     // Preciso de um modo para armazenar as medidas anteriores para montar as condições corretas de mudança de estado.
     // Por enquanto vou utilizar simples structs.
-    float memory_accel_z[3] = {0}, memory_bar[6] = {0}, average_pressure[2] = {0};
-    int cycles_1 = 0, cycles_2 = -1;
+    float memory_accel_z[3] = {0}, memory_bar[2] = {0}, last_delta_H = 0;
+    int cycles_1 = 0, K = 1;
+    float const_1, const_2, const_3, const_4; // Algum valor 
     while(1) { // Loop principal do controlador
         SensorData data = read_sensors(); // Struct recebe as leituras
                                           // Estou considerando que essa leitura ocorre a cada 0,1s
         memory_accel_z[cycles_1%3] = data.accel_z;
         memory_bar[1] = data.pressure;
-
-
-        /*
-        // Enquanto não estiver no COASTING (cycles_2 == -1), apenas preenche [0], [1] e [2]
-        if(cycles_2 == -1) memory_bar[cycles_1%3] = data.pressure; // Apenas [0], [1] e [2]
-        else memory_bar[cycles_2%3 + 3] = data.pressure;         // Apenas [3], [4] e [5]
-
-        if(current_state == STATE_COASTING){
-            if(cycles_2 == 0) {
-                // average_pressure[0] sempre será a média mais antiga
-                average_pressure[0] = (memory_bar[0] + memory_bar[1] + memory_bar[2])/3;
-                average_pressure[1] = (memory_bar[3] + memory_bar[4] + memory_bar[5])/3;
-            }
-            else average_pressure[1] = (memory_bar[3] + memory_bar[4] + memory_bar[5])/3;
-        }
-        */
         
 
         switch (current_state){
@@ -78,6 +64,31 @@ void main(void){
                 break;
             
             case STATE_POWERED_FLIGHT:
+                
+                // 1. ATUALIZAÇÃO DO last_delta_H
+                float delta_H_meas, delta_H_phy, delta_H_update, delta_H_min_1;
+
+                // 1.1 Delta medido pelo barômetro
+                delta_H_meas = fabs(memory_bar[1] - memory_bar[0]);
+
+                // 1.2 Limite físico baseado na aceleração
+                //    ΔH ≈ 0.5 * a * dt²  (dt = 0.01s)
+                delta_H_phy = 0.5f * fabs(data.accel_z) * 0.0001f; // Ainda preciso aprimorar essa fórmulas
+
+                // 1.3 Impor mínimo físico
+                if(delta_H_phy < delta_H_min_1)
+                    delta_H_phy = delta_H_min_1; // Verificar se será necessário um limite mínimo
+
+                // 1.4 Atualização controlada
+                if(delta_H_meas <= delta_H_phy)
+                    delta_H_update = delta_H_meas;
+                else
+                    delta_H_update = delta_H_phy;
+
+                // 1.5 Atualizar memória do barômetro
+                memory_bar[0] = memory_bar[1];
+
+                // 2. Lógica para mudar de estado
                 int flag_coasting = 0;
                 for(int j = 0; j < 3; j++){
                     // Acho que não preciso me preocupar com medidas (ruídos) positivas aqui
@@ -86,20 +97,40 @@ void main(void){
                     else flag_coasting = 0;
                 }
                 if(flag_coasting == 3) current_state = STATE_COASTING;
-                cycles_2 = 0; //"Inicia" o ciclo 2
+                
+
                 break;
 
             case STATE_COASTING:
+                
                 // 1. Registro das alturas (pressões) com filtro
                 // Adicionar lógica para obter o primeiro valor válido, memory_bar[0];
-                int delta_H_limit;
+                float delta_H_adapt, delta_H_min_2, delta_H_phy, delta_H_limit;
+                
+                // 2. Filtro físico
+                delta_H_min_2; // Algum valor 
+                delta_H_phy = (const_4)*exp(-K/const_1); // Decaimento exponencial da velocidade
+                if(delta_H_phy >= delta_H_min_2) delta_H_limit = delta_H_phy;
+                else delta_H_limit = delta_H_min_2;
+
+                // 3. Filtro adaptativo
+                delta_H_adapt =  const_2*last_delta_H + const_3;
+
+                // 4. Híbrido
+                if(delta_H_phy > delta_H_adapt) delta_H_limit = delta_H_adapt;
+
                 if(abs(memory_bar[0] - memory_bar[1]) <= delta_H_limit) {
                     // memory_bar[0] - valor validado
                     // memory_bar[1] - valor em análise
+                    last_delta_H = abs(memory_bar[0] - memory_bar[1]);
                     memory_bar[0] = memory_bar[1];
                 }
-                else {memory_bar[0] += delta_H_limit;}
+                else {
+                    last_delta_H = delta_H_limit;
+                    memory_bar[0] += delta_H_limit;
+                }
                 
+                K++;
                 break;
             
             case STATE_APOGEE:
